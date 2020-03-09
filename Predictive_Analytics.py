f5ebe143be8544dc787a5ed8bdeb28b8c0e1d6c2
anujns <47839322+anujns@users.noreply.github.com>
2020-03-09 19:14:32 -0400

    Update Predictive_Analytics.py

--- Predictive_Analytics.py
+++ Predictive_Analytics.py
@@ -72,71 +72,95 @@ def Kmeans(X_train,N):
 
 def SklearnSupervisedLearning(X_train,Y_train,X_test):
     
-    from sklearn.metrics import accuracy_score
-    from sklearn.metrics import confusion_matrix
-    from sklearn.model_selection import GridSearchCV
+    results = []
     
     # Scaling
-    from sklearn.preprocessing import MinMaxScaler
-    sc= MinMaxScaler()
-    x_train = sc.fit_transform(x_train)
-    x_test = sc.transform(x_test)
+    sc = sklearn.preprocessing.MinMaxScaler()
+    x_train = sc.fit_transform(X_train)
+    x_test = sc.transform(X_test)
     
     # Training SVM
-    from sklearn.svm import SVC
-    svc_clf=SVC(kernel ='linear', C=1, gamma=1)
-    svc_clf.fit(x_train, y_train)
-    y_pred_svc= svc_clf.predict(x_test)
-    print(accuracy_score(y_test, y_pred_svc))
     
-    cm_svc = confusion_matrix(y_test, y_pred_svc)
-    print(cm_svc)
+    svc_clf=SVC(kernel ='linear', C=1, gamma=1)
+    svc_clf.fit(x_train, Y_train)
+    y_pred_svc= svc_clf.predict(x_test)
+    print("SVM Accuracy: " + str(sklearn.metrics.accuracy_score(Y_test, y_pred_svc) * 100))
+    
+    
     
     # confusion matrix - SVM
-    labels = ["A","B","C","D","E","F"]
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    cax = ax.matshow(cm_svc)
-    plt.title('Confusion matrix of the classifier')
-    fig.colorbar(cax)
-    ax.set_xticklabels([''] + labels)
-    ax.set_yticklabels([''] + labels)
-    plt.xlabel('Predicted')
-    plt.ylabel('True')
-    plt.show()
+    cm_svc = confusion_matrix(Y_test, y_pred_svc) 
+    
+    
+    # Training KNN
+    knn_model = KNeighborsClassifier(n_neighbors=5)
+    knn_model.fit(x_train, Y_train)
+    y_pred_knn = knn_model.predict(X_test)
+    print("KNN Accuracy: " + str(sklearn.metrics.accuracy_score(Y_test, y_pred_knn) * 100))
+    
+    
+    # Confusion Matrix - KNN
+    cm_knn = confusion_matrix(y_test, y_pred_knn)
+    
+    
+    # Decision Tree
+    dt = DecisionTreeClassifier(max_leaf_nodes=50, random_state=0)
+    dt.fit(x_train, Y_train)
+    y_pred_tree = dt.predict(x_test)
+    print("Decision Tree Accuracy: " + str(sklearn.metrics.accuracy_score(Y_test, y_pred_tree) * 100))
+    
+    # Confusion Matrix - DTree
+    cm_tree = confusion_matrix(y_test, y_pred_tree)
+    
+    # Logistic Regression Model
+    logclf = LogisticRegression(random_state = 0, penalty = 'l1', solver='saga', class_weight='balanced', multi_class='multinomial').fit(x_train, Y_train)
+    y_pred_log = logclf.predict(x_test)
+    print("Logistic Regression Accuracy: " + str(sklearn.metrics.accuracy_score(Y_test, logclf) * 100))
+    
+    return results
+
+def SklearnVotingClassifier(X_train,Y_train,X_test):
+    
+    eclf1 = VotingClassifier(estimators=[('lr', logclf), ('dt', dt), ('knn', knn_model), ('svc', svc_clf)], voting='hard')
+    eclf1 = eclf1.fit(X_train, Y_train)
+    y_ens = logclf.predict(X_test)
+    print("Ensemble Model Accuracy: " + str(sklearn.metrics.accuracy_score(Y_test, y_ens) * 100))
+
+
+def GridSearchSVM(X_train, Y_train):
+
+    # Scaling
+    sc = sklearn.preprocessing.MinMaxScaler()
+    x_train = sc.fit_transform(X_train)
+    
     
     # Grid Search - SVM
     parameters = {'kernel':['linear'], 'C':[0.00001, 0.0001,0.001,0.01,0.1, 0.5, 0.8]}
     # parameters = {'kernel':['linear'], 'C':[0.1,10,20,40,70,100,200,260,512]}
-    clfGridSV = GridSearchCV(clf1,parameters,cv=3)
-    clfGridSV.fit(x_train,y_train)
+    clfGridSV = GridSearchCV(svc_clf,parameters,cv=3)
+    clfGridSV.fit(X_train,Y_train)
     accuracy_SVM=clfGridSV.cv_results_['mean_test_score']
     plt.ylabel('Accuracy Of Linear Kernel SVM')
     plt.xlabel('Regularization Parameter (C)')
     plt.plot([0.00001, 0.0001,0.001,0.01,0.1, 0.5, 0.8],accuracy_SVM)
+   
     
-    # Training KNN
-    from sklearn.neighbors import KNeighborsClassifier
-    knn_model = KNeighborsClassifier(n_neighbors=5)
-    knn_model.fit(x_train, y_train)
-    y_pred_knn = knn_model.predict(x_test)
+def GridSearchSVM(X_train, Y_train):
+    
+    # Scaling
+    sc = sklearn.preprocessing.MinMaxScaler()
+    x_train = sc.fit_transform(X_train)
+    
+    # Grid Search - DTree
+    parameters = {'max_depth': [3,6,9,12]}
+    gs_tree = GridSearchCV(estimator,parameters,cv=3)
+    gs_tree.fit(x_train,y_train)
+    accuracy_gs_tree=gs_tree.cv_results_['mean_test_score']
+    plt.ylabel('Accuracy Of Decision Tree')
+    plt.xlabel('Regularization Parameter (max_depth)')
+    plt.plot([3,6,9,12],accuracy_gs_tree)
 
-    print(accuracy_score(y_test, y_pred_knn))
-    cm_knn = confusion_matrix(y_test, y_pred_knn)
-    print(cm_knn)
-    
-    # Confusion Matrix - KNN
-    labels = ["A","B","C","D","E","F"]
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    cax = ax.matshow(cm_knn)
-    plt.title('Confusion matrix of the classifier')
-    fig.colorbar(cax)
-    ax.set_xticklabels([''] + labels)
-    ax.set_yticklabels([''] + labels)
-    plt.xlabel('Predicted')
-    plt.ylabel('True')
-    plt.show()
+def GridSearchKNN(X_train, Y_train):
     
     # Grid Search - KNN
     parameters = {'n_neighbors': [1,3,5,10,15,30]}
@@ -146,20 +170,7 @@ def SklearnSupervisedLearning(X_train,Y_train,X_test):
     plt.ylabel('Accuracy Of KNN')
     plt.xlabel('Regularization Parameter (n_neighbor)')
     plt.plot([1,3,5,10,15,30],accuracy_gs_knn)
-    
-    
-    
-    
 
-def SklearnVotingClassifier(X_train,Y_train,X_test):
-    
-    """
-    :type X_train: numpy.ndarray
-    :type X_test: numpy.ndarray
-    :type Y_train: numpy.ndarray
-    
-    :rtype: List[numpy.ndarray] 
-    """
 
 
 """
